197e9a19986ea3875b3698c6760dfad758eb5a3d
Cihan <ozcancihan714@gmail.com>
2023-04-09 19:24:08 +0300

    Version Odev

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer style={{ backgroundColor: "var(--primary-color)", color: "#fff", textAlign: "center", position: "fixed", bottom: 0, width: "100vw" }}>
-            <h5>Cihan Özcan --- ozcancihan714@gmail.com</h5>
+            {/* <h5>Cihan Özcan --- ozcancihan714@gmail.com</h5>  */}
+            <h5>BlogAnz is a platform for sharing interesting and engaging content on a wide range of topics. For more information, please visit our website.</h5>
         </footer>
     )
 }
